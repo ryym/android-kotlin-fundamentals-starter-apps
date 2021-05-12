f8ef0f701e916cc7cab40df1e12f03a7a1dd52fc
ryym <ryym.64@gmail.com>
2021-05-12 12:15:23 +0900

    refactor(guesstheword): Change GameViewModel fields to LiveData

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,14 +1,15 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     /** The current word */
-    var word = ""
+    val word = MutableLiveData<String>("")
 
     /** The current score */
-    var score = 0
+    val score = MutableLiveData<Int>(0)
 
     /** The list of words - the front of the list is the next word to guess */
     private lateinit var wordList: MutableList<String>
@@ -25,12 +26,12 @@ class GameViewModel : ViewModel() {
     }
 
     fun onSkip() {
-        score--
+        score.value = score.value?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value = score.value?.plus(1)
         nextWord()
     }
 
@@ -70,7 +71,7 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         if (wordList.isNotEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 }
